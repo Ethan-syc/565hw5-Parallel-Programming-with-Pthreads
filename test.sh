d7e222895611f5cacfaa5845454962a6940d911a
YichuanShi <yichuan.shi@duke.edu>
2018-11-21 06:07:04 -0500

    out put file

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..10000}; do
   ./rainfall 8 50 0.5 4096 measurement_4096x4096.in > result.out
-  ./check.py 4096 sample_512x512.out result.out
+  ./check.py 4096 measurement_4096x4096.out result.out
 done
