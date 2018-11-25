085fd13d33c297106082aeaf7efbbc4a4f84745f
Yichuan Shi <yichuan.shi@duke.edu>
2018-11-25 04:56:50 +0000

    add report and test result

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ for i in {1..4}; do
   ./rainfall $j 50 0.5 4096 measurement_4096x4096.in > result_$j.out
   echo "using $j threads:"
   ./check.py 4096 measurement_4096x4096.out result_$j.out
+  head -n3 result_$j.out
   let "j = j * 2"
 done
